7bf59cc292c2de6ac1f48ad7d26795020b031679
Keshav <keshavpal748@gmail.com>
2025-08-31 16:37:11 +0530

    new update

---
+++ DivideConquer/test.java
@@ -0,0 +1,90 @@
+package DivideConquer;
+
+public class test {
+
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void mergeSort(int arr[], int si, int ei){
+        if (si >= ei) {
+            return;
+            
+        }
+        int mid = si + (ei - si) / 2;
+        mergeSort(arr, si, mid); // left part
+        mergeSort(arr, mid + 1, ei); // right part
+        merge(arr,si,mid,ei);
+    }
+
+
+
+
+
+    private static void merge(int[] arr, int si, int mid, int ei) {
+        int  temp[] = new int[ei - si + 1];
+        int i = si;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= ei) {
+            if (arr[i] < arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= ei) {
+            temp[k++] = arr[j++];
+        }
+
+        for (i = si, k = 0; i <= ei; i++, k++) {
+            arr[i] = temp[k];
+        }
+    }
+
+    public static void quickSort(int arr[], int si, int ei){
+        if (si >= ei) {
+            return;
+        }
+        int pIdx = partition(arr, si, ei);
+        quickSort(arr, si, pIdx - 1); // left part
+        quickSort(arr, pIdx + 1, ei); // right part
+    }
+
+    public static int partition(int[] arr, int si, int ei) {
+        int pivot = arr[ei];
+        int i = si - 1;
+
+        for (int j = si; j < ei; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        // Swap arr[i + 1] and arr[ei] (or pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[ei];
+        arr[ei] = temp;
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {6,3,9,5,2,8,-5};
+        // mergeSort(arr, 0, arr.length - 1);
+        quickSort(arr, 0, arr.length - 1);
+        printArr(arr);
+    }
+}
