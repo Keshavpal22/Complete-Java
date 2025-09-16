a7e9675108c4d900685b8f279872ab2b96ee1f91
Keshav <keshavpal748@gmail.com>
2025-09-17 00:13:23 +0530

    new update

--- Backtracking/test.java
+++ Backtracking/test.java
@@ -7,7 +7,6 @@ public class test {
         }
 
         // recursion
-
         arr[i] = val;
         changeArr(arr, i + 1, val + 1);
         arr[i] = arr[i] - 2;
@@ -34,12 +33,79 @@ public class test {
         }
     }
 
+    public static boolean isSafe(char board[][], int row, int col){
+        // vertical up
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+        // diagonal left up
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        // diagonal right up
+        for(int i=row-1,j=col+1;i>=0 && j<board.length;i--,j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean nQueens(char board[][], int row){
+        //base case
+        if(row == board.length){
+            // printBoard(board);
+            count++;
+            return true;
+        }
+
+        //recursion
+        for(int j=0;j<board.length;j++){
+            if(isSafe(board, row, j)){
+                board[row][j] = 'Q';
+                if (nQueens(board, row + 1)) {
+                    return true;
+                    
+                }; // function call
+                board[row][j] = '.'; // backtracking step
+            }
+           
+        }
+        return false;
+    }
+
+    public static void printBoard(char board[][]){
+        System.out.println("-----Chess Board-----");
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static int count = 0;
+
+    // ✅ main is inside the class now
     public static void main(String[] args) {
-        // int arr[] = {1,2,3,4,5};
-        // arr = new int[]{ 1, 2, 3, 4, 5 }; // [1,2,3,4,5]
-        // changeArr(arr, 0, 1);
-        // printArr(arr);
-        String str = "abcd";
-        findPermutations(str, "");
+        int n=4;
+        char board[][] = new char[n][n];
+        //Initialize the board
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j] = '.';
+            }
+        }
+        if (nQueens(board, 0)) {
+            System.out.println("Solution is possible");
+            printBoard(board);
+        } else {
+            System.out.println("Solution not possible");
+        }
+        // System.out.println("Total ways to solve N-Queens problem: " + count);
     }
 }
